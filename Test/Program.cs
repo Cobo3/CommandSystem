4fd21d0b1b43468cbfacf5884ccd82864bf16049
Cobo <antoniocogo@gmail.com>
2017-02-15 06:49:10 +0100

    created test project

---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using SickDev.CommandSystem;
+
+namespace Test {
+    class Program {
+        static void Main(string[] args) {
+            CommandsManager manager = new CommandsManager();
+            CommandsManager.onMessage += Console.WriteLine;
+            manager.Load();
+            Console.WriteLine(manager.GetCommandExecuter("DomainInfo").Execute());
+            while (true)
+                Thread.Sleep(5000);
+        }
+
+        [Command]
+        static string DomainInfo() {
+            return AppDomain.CurrentDomain.ToString();
+        }
+    }
+}
