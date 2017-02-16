10611acebab0f53edaf0dc143508597f35c77512
Cobo <antoniocogo@gmail.com>
2017-02-16 06:14:36 +0100

    fix

--- Source/Config.cs
+++ Source/Config.cs
@@ -1,16 +1,14 @@
-﻿using System.Reflection;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace SickDev.CommandSystem{
     internal class Config{
+        List<string> _assembliesWithCommands = new List<string> {
+           "CommandSystem.dll",
+        };
+
         public string[] assembliesWithCommands{
             get { return _assembliesWithCommands.ToArray(); }
         }
-
-        List<string> _assembliesWithCommands = new List<string> {
-           "CommandSystem.dll",
-           Assembly.GetEntryAssembly().ManifestModule.Name
-        };
 
         public void AddAssemblyWithCommands(string assembly) {
             _assembliesWithCommands.Add(assembly);
