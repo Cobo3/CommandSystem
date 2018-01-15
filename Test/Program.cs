39e4165f75b0eb891236ca348795790d6336666a
Cobo <antoniocogo@gmail.com>
2018-01-15 18:22:36 +0100

    Added Signing

--- Test/Program.cs
+++ Test/Program.cs
@@ -3,18 +3,23 @@ using SickDev.CommandSystem;
 
 namespace Test {
     class Program {
+        static CommandsManager manager;
+        static Command command;
         static void Main(string[] args) {
             CommandsManager.onMessage += Console.WriteLine;
             CommandsManager.onExceptionThrown += exception=>Console.WriteLine(exception.Message);
 
             Configuration configuration = new Configuration("Test");
-            CommandsManager manager = new CommandsManager(configuration);
+            manager = new CommandsManager(configuration);
             manager.LoadCommands();
-            manager.Add(new CommandsBuilder(typeof(Program)).Build());
-            Console.WriteLine(manager.Execute("Max (int)2 3"));
+            //manager.Add(new CommandsBuilder(typeof(Program)).Build());
+            manager.Add(command = new FuncCommand<float, float, float>(Max));
+            Console.WriteLine(manager.Execute("Max 2 3"));
+            Console.WriteLine(manager.Execute("Max 2 3"));
         }
 
         public static float Max(float a, float b) {
+            manager.Remove(command);
             if(a > b)
                 return a;
             else
